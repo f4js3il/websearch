ab25b7157cf3c77b84ba08e98ec8821b6b325bba
Shalu Chandran <shaluchandran@yahoo.co.in>
2025-10-14 19:48:37 -0500

    anthropic search options

--- src/main/java/com/web/websearch/service/WebSearchService.java
+++ src/main/java/com/web/websearch/service/WebSearchService.java
@@ -4,10 +4,10 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.openai.api.OpenAiApi.ChatCompletionRequest.WebSearchOptions;
+import org.springframework.ai.anthropic.api.AnthropicApi;
+import org.springframework.ai.anthropic.AnthropicChatModel;
+import org.springframework.ai.anthropic.AnthropicChatOptions;
+import org.springframework.ai.anthropic.api.AnthropicApi.ChatCompletionRequest.ThinkingConfig;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,34 +16,32 @@ import java.util.Map;
 
 @Service
 public class WebSearchService {
-    private final static String API_KEY = System.getenv("OPENAI_API_KEY");
+    private final static String API_KEY = System.getenv("ANTHROPIC_API_KEY");
 
-    private final OpenAiChatOptions universalOpenAiChatOptions = OpenAiChatOptions
+    private final AnthropicChatOptions anthropicChatOptions = AnthropicChatOptions
                                                         .builder()
-                                                        .model("gpt-4o-mini-search-preview")
+                                                        .model("claude-sonnet-4-5-20250929")
+                                                        .maxTokens(500)
                                                         .build();
-    private final OpenAiChatModel openAiChatModel = OpenAiChatModel
-                                                    .builder()
-                                                    .openAiApi(OpenAiApi.builder().apiKey(API_KEY).build())
-                                                    .defaultOptions(universalOpenAiChatOptions)
+    private final AnthropicChatModel anthropicChatModel = AnthropicChatModel
+                                                    .builder().anthropicApi(AnthropicApi.builder().apiKey(API_KEY).build())
+                                                    .defaultOptions(anthropicChatOptions)
                                                     .build();
-    private final ChatClient chatClient= ChatClient.builder(openAiChatModel).build();
+    private final ChatClient chatClient= ChatClient.builder(anthropicChatModel).build();
 
 
     public String search(String query) {
-       WebSearchOptions webSearchOptions = new WebSearchOptions(WebSearchOptions.SearchContextSize.LOW, null);
-        OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder().webSearchOptions(webSearchOptions).build();
         PromptTemplate userPromptTemplate = new PromptTemplate("Give me description of operations of firm {firm}");
         Message userMessage = userPromptTemplate.createMessage(Map.of("firm", query));
         PromptTemplate systemPromptTemplate = new PromptTemplate("You are a workers compensation premium audit doing audits of business firms");
         Message systemMessage = systemPromptTemplate.createMessage(Map.of());
         return chatClient.prompt(new Prompt(List.of(userMessage,systemMessage)))
-                .options(openAiChatOptions).call().content();
+                .call().content();
     }
 
     public String getBestRestaurants(String cuisine, String city, String state) {
-        WebSearchOptions webSearchOptions = new WebSearchOptions(WebSearchOptions.SearchContextSize.LOW, new WebSearchOptions.UserLocation("approximate", new WebSearchOptions.UserLocation.Approximate(city,"US",state,"America/Chicago")));
-        OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder().webSearchOptions(webSearchOptions).build();
+        ThinkingConfig webSearchOptions = new ThinkingConfig(AnthropicApi.ThinkingType.ENABLED, null);
+        AnthropicChatOptions openAiChatOptions = AnthropicChatOptions.builder().thinking(webSearchOptions).build();
         return chatClient.prompt(new Prompt("Provide me best restaurants for " + cuisine ))
                 .options(openAiChatOptions).call().content();
     }
